096bedaac83e03604a69f406f6dc3b2937c06c36
Jared Grippe <techops+jared@change.org>
2011-10-13 14:07:45 -0700

    wip

--- lib/test_balancer.rb
+++ lib/test_balancer.rb
@@ -40,52 +40,10 @@ class TestBalancer
     # TODO: take execution_time into account
     tests.in_groups(n, false).map{ |tests|
       tests.group_by(&:class).map{|test_class, tests_subset|
-        "#{test_class::COMMAND} #{tests_subset.join(' ')}"
+        # "#{test_class::COMMAND} #{tests_subset.join(' ')}"
+        "#{test_class::COMMAND} #{tests_subset.first(2).join(' ')}" #TEMP for faster development
       }.join('; ')
     }
-  end
-
-
-  # def all
-  #   @all ||= features + specs
-  # end
-  #
-  # def features
-  #   @features ||= Features.all(root)
-  # end
-  #
-  # def specs
-  #   @specs ||= Specs.all(root)
-  # end
-  #
-  # # splits all tests into n groups based on their type (cucumber,spec,etc)
-  # # TODO: add magic balancing based on execution time here
-  # def balance_for n
-  #   return [all.clone] if n == 1
-  #   # debugger;1
-  #   classes = all.map(&:class).uniq
-  #   tests_by_class = all.group_by(&:class)
-  #   groups_per_class = n / classes.length
-  #   remainder = n - (groups_per_class * classes.length)
-  #
-  #   groups = {}
-  #   classes.each{ |klass, tests| groups[klass] = groups_per_class }
-  #
-  #   if remainder > 0 # add remainder to the largest class of tests
-  #     # for now assume features are slower
-  #     groups[TestBalancer::Features::Test] += remainder
-  #     # klass = tests_by_class.sort_by{|(k,v)| v.length}.last.first
-  #     # puts "#{klass} += #{remainder}"
-  #     # groups[klass] += remainder
-  #   end
-  #
-  #   groups.values.inject(&:+) == n or raise "BUG!"
-  #
-  #   tests_by_class.each{|klass, tests|
-  #     groups[klass] = TestBalancer::Tests.new(tests).in_groups(groups[klass], false)
-  #   }
-  #
-  #   groups.values.flatten(1)
-  # end
+  end``
 
 end
