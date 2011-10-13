86d0cb43a8d08d588a0f9005b0d7a911f23dc377
Jared Grippe <techops+jared@change.org>
2011-10-13 15:06:08 -0700

    wip

--- lib/test_balancer/bundler.rb
+++ lib/test_balancer/bundler.rb
@@ -2,9 +2,12 @@ module TestBalancer::Bundler
 
   def self.with_clean_env
     Bundler.with_clean_env{
+
+      # this fixes a bug in older version of bundler
       ENV.delete("BUNDLE_BIN_PATH")
       ENV.delete("BUNDLE_GEMFILE")
       ENV["RUBYOPT"] = ENV["RUBYOPT"].gsub('-rbundler/setup', ' ') if ENV["RUBYOPT"]
+
       yield
     }
   end
