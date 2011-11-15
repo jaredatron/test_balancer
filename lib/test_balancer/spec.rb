085a28383f3ed2f9825466bf691c6ec434e42702
Jared Grippe <techops+jared@change.org>
2011-11-14 18:42:08 -0800

    WIP

--- lib/test_balancer/spec.rb
+++ lib/test_balancer/spec.rb
@@ -3,11 +3,11 @@ class TestBalancer::Spec < TestBalancer::Test
 
   # TODO remove bundler dependency
   # TODO switch back to progress
-  COMMAND = "bundle exec spec --format specdoc --format specdoc:log/spec.log"
+  COMMAND = "bundle exec rspec --format documentation --format documentation:log/spec.log"
 
   def self.all root
     root = Pathname.new(root)
-    return Dir[root.join('spec/**/*_spec.rb')].map{ |path|
+    Dir[root.join('spec/**/*_spec.rb')].map{ |path|
       new(Pathname.new(path).relative_path_from(root))
     }
   end
